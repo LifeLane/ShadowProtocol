38896f7c748d14d56371e7c8388cc18c11b958df
Rajib Singh <connectedtorajib@gmail.com>
2025-07-29 09:52:46 +0000

    use icons instead of the images for the scrollbar, its not loading the i

--- src/components/common/LinkMarquee.tsx
+++ src/components/common/LinkMarquee.tsx
@@ -2,18 +2,18 @@
 "use client"
 
 import Link from 'next/link';
-import Image from 'next/image';
+import { LinkIcon } from 'lucide-react';
 
 const links = [
-    { href: "https://gmgn.ai/sol/token/solscan_B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/gmgn.e55ae73f.png", name: "Solscan" },
-    { href: "https://app.bubblemaps.io/sol/token/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/bubblemaps.fa27f170.png", name: "Bubblemaps" },
-    { href: "https://t.me/BlumCryptoTradingBot?start=ia0HoVfVWL_T_B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/blum.1f1f07ca.png", name: "Telegram" },
-    { href: "https://www.geckoterminal.com/solana/tokens/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/geckoterminal.bcf76b35.webp", name: "GeckoTerminal" },
-    { href: "https://dexscreener.com/solana/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/dexscreener.e36090e0.png", name: "DexScreener" },
-    { href: "https://rugcheck.xyz/tokens/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/rugcheck.43bc469c.jpg", name: "RugCheck" },
-    { href: "https://photon-sol.tinyastro.io/en/r/solscanofficial/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/photon.6a796ee9.webp", name: "Photon" },
-    { href: "https://www.dextools.io/app/en/solana/pair-explorer/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", imgSrc: "https://solscan.io/_next/static/media/dexTools.2ba36145.png", name: "DEXTools" },
-    { href: "https://birdeye.so/token/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR?chain=solana", imgSrc: "https://solscan.io/_next/static/media/birdeye.092090c8.png", name: "Birdeye" }
+    { href: "https://gmgn.ai/sol/token/solscan_B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "Solscan" },
+    { href: "https://app.bubblemaps.io/sol/token/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "Bubblemaps" },
+    { href: "https://t.me/BlumCryptoTradingBot?start=ia0HoVfVWL_T_B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "Telegram" },
+    { href: "https://www.geckoterminal.com/solana/tokens/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "GeckoTerminal" },
+    { href: "https://dexscreener.com/solana/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "DexScreener" },
+    { href: "https://rugcheck.xyz/tokens/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "RugCheck" },
+    { href: "https://photon-sol.tinyastro.io/en/r/solscanofficial/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "Photon" },
+    { href: "https://www.dextools.io/app/en/solana/pair-explorer/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "DEXTools" },
+    { href: "https://birdeye.so/token/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR?chain=solana", name: "Birdeye" }
 ];
 
 const LinkMarquee = () => {
@@ -22,7 +22,7 @@ const LinkMarquee = () => {
             <div className="flex animate-marquee-infinite">
                 {links.concat(links).map((link, index) => (
                     <Link href={link.href} key={index} target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 mx-6 flex-shrink-0 text-muted-foreground hover:text-primary transition-colors duration-300">
-                        <Image src={link.imgSrc} alt={`${link.name} logo`} className="rounded-full" width={20} height={20} unoptimized />
+                        <LinkIcon className="w-4 h-4" />
                         <span className="font-bold text-sm">{link.name}</span>
                     </Link>
                 ))}

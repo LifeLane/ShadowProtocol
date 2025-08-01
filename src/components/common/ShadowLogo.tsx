f5a1be0e42a3190e96f4f593d29cf27293234e6e
Rajib Singh <connectedtorajib@gmail.com>
2025-08-01 14:21:44 +0000

    make sure the logo is entirely visible on small screens,

--- src/components/common/ShadowLogo.tsx
+++ src/components/common/ShadowLogo.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const ShadowLogo = () => (
     <motion.pre
-        className="text-primary glow font-bold text-center text-[clamp(8px,5vw,24px)] leading-[0.8] tracking-tighter sm:tracking-normal w-full overflow-hidden"
+        className="text-primary glow font-bold text-center text-[clamp(4px,3.5vw,24px)] leading-[0.8] tracking-tighter w-full overflow-hidden"
         initial={{ opacity: 0, scale: 0.5, y: 50 }}
         whileInView={{ opacity: 1, scale: 1, y: 0 }}
         viewport={{ once: true, amount: 0.8 }}

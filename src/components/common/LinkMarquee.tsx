9955343cf9def6df4b53b2d19d447528a9d36a05
Rajib Singh <connectedtorajib@gmail.com>
2025-08-01 14:20:42 +0000

    please update the marquee and token manifest links properly
    #SHADOW Bl

--- src/components/common/LinkMarquee.tsx
+++ src/components/common/LinkMarquee.tsx
@@ -10,11 +10,11 @@ const links = [
     { href: "https://gmgn.ai/sol/token/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "GMGN", icon: GitFork },
     { href: "https://dex.coinmarketcap.com/token/solana/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR/", name: "CoinMarketCap", icon: TrendingUp },
     { href: "#", name: "Telegram", icon: Send },
-    { href: "https://www.geckoterminal.com/solana/pools/AY53pXFjCMbFpQ9yTT2s2T61v9a8yY26b1hFME5kZW9r", name: "GeckoTerminal", icon: Terminal },
-    { href: "https://dexscreener.com/solana/ay53pxfjcmbfpq9ytt2s2t61v9a8yy26b1hfme5kzw9r", name: "DexScreener", icon: BarChart2 },
+    { href: "https://www.geckoterminal.com/solana/tokens/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "GeckoTerminal", icon: Terminal },
+    { href: "https://dexscreener.com/solana/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "DexScreener", icon: BarChart2 },
     { href: "https://rugcheck.xyz/tokens/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "RugCheck", icon: ShieldCheck },
-    { href: "https://photon-sol.tinyastro.io/en/r/solano/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "Photon", icon: Zap },
-    { href: "https://www.dextools.io/app/en/solana/pair-explorer/AY53pXFjCMbFpQ9yTT2s2T61v9a8yY26b1hFME5kZW9r", name: "DEXTools", icon: LinkIcon },
+    { href: "https://photon-sol.tinyastro.io/en/r/solscanofficial/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "Photon", icon: Zap },
+    { href: "https://www.dextools.io/app/en/solana/pair-explorer/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR", name: "DEXTools", icon: LinkIcon },
     { href: "https://birdeye.so/token/B6XHf6ouZAy5Enq4kR3Po4CD5axn1EWc7aZKR9gmr2QR?chain=solana", name: "Birdeye", icon: Eye }
 ];
 
